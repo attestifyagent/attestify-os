97184ca41671247e7eb52e352ceb80f89d2ced2a
attestifyagent <attestifyagent@gmail.com>
2026-05-04 22:57:50 +0100

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+'use client';
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>agentic.market Dashboard</h1>
+      <p>Test your loops here</p>
+      {/* Add a simple UI with fetch calls */}
+    </div>
+  );
+}
